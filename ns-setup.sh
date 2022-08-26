4f72b9f2ae8200125271770e0e198605830b3990
boshetyn <74248125+boshetyn@users.noreply.github.com>
2022-08-26 15:44:16 +0300

    Update ns-setup.sh

--- ns-setup.sh
+++ ns-setup.sh
@@ -14,7 +14,8 @@ sudo mkdir -p /nstst003/keyrings
 #curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /nstst003/keyrings/docker.gpg
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  #"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/nstst003/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   #$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   $(lsb_release -cs) stable" | sudo tee /nstst003/sources.list.d/docker.list > /dev/null
 
