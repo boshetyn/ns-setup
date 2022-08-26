6ff1ab8745ded2151033de86853f3033355f371c
boshetyn <74248125+boshetyn@users.noreply.github.com>
2022-08-26 10:11:09 +0300

    Update and rename ns-setup.sh to ns-setup1.sh
    test ns file for test set API pass function

---
+++ ns-setup1.sh
@@ -0,0 +1,12 @@
+
+
+
+echo Придумайте АПИ-серкет
+read secret
+
+
+echo "Your secret for Nightscout access (write it down!):"
+echo "Ваш секретный ключ для доступа к Nightscout (запишите!):"
+echo "secret: $secret"
+echo "email: $email"
+echo "domain: $domain"
